85dcbe6ec65edfb1fde09a53f68c530cdde60c37
Vitalii Malynka <vitalii.malynka@gmail.com>
2023-11-27 20:40:01 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cp-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'testing-web-app';
+}
