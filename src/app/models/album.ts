13bc23bf1d3bccb233fddb9801de50328fb0e900
budanov <budanov@rozetka.ua>
2023-11-29 20:43:32 +0200

    add album interface and class

---
+++ src/app/models/album.ts
@@ -0,0 +1,13 @@
+import {IAlbum} from "../interfaces/albums.interface";
+
+export class Album implements IAlbum {
+  public readonly id: number = 0;
+  public title: string = '';
+  public userId: number = 0;
+
+  constructor( obj: IAlbum) {
+    this.id = obj.id;
+    this.title = obj.title;
+    this.userId = obj.userId;
+  }
+}
