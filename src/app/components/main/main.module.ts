d9ae069bc7cac552c2afb38e6415dd776b3371a3
Vitalii Malynka <vitalii.malynka@gmail.com>
2023-11-29 20:58:45 +0200

    add main module

---
+++ src/app/components/main/main.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { MainComponent } from './main.component';
+
+
+
+@NgModule({
+  declarations: [
+    MainComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule
+  ]
+})
+export class MainModule { }
